d505d32340532e5d11dacfa5e4e36a70e4177ba9
Alexandr Trostyanko <Aleksandr.Trostyanko@itechart-group.com>
2021-08-03 09:46:07 +0300

    Добавлен Builder паттерн

---
+++ src/main/java/patterns/builder/builders/CarManualBuilder.java
@@ -0,0 +1,59 @@
+package patterns.builder.builders;
+
+import patterns.builder.cars.CarType;
+import patterns.builder.cars.Manual;
+import patterns.builder.components.Engine;
+import patterns.builder.components.GPSNavigator;
+import patterns.builder.components.Transmission;
+import patterns.builder.components.TripComputer;
+
+/**
+ * В отличие от других создающих паттернов, Строители могут создавать совершенно
+ * разные продукты, не имеющие общего интерфейса.
+ *
+ * В данном случае мы производим руководство пользователя автомобиля с помощью
+ * тех же шагов, что и сами автомобили. Это устройство позволит создавать
+ * руководства под конкретные модели автомобилей, содержащие те или иные фичи.
+ */
+public class CarManualBuilder implements Builder {
+    private CarType type;
+    private int seats;
+    private Engine engine;
+    private Transmission transmission;
+    private TripComputer tripComputer;
+    private GPSNavigator gpsNavigator;
+
+    @Override
+    public void setCarType(CarType type) {
+        this.type = type;
+    }
+
+    @Override
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    @Override
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    @Override
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+
+    @Override
+    public void setTripComputer(TripComputer tripComputer) {
+        this.tripComputer = tripComputer;
+    }
+
+    @Override
+    public void setGPSNavigator(GPSNavigator gpsNavigator) {
+        this.gpsNavigator = gpsNavigator;
+    }
+
+    public Manual getResult() {
+        return new Manual(type, seats, engine, transmission, tripComputer, gpsNavigator);
+    }
+}
---
+++ src/main/java/patterns/builder/cars/Manual.java
@@ -0,0 +1,48 @@
+package patterns.builder.cars;
+
+import patterns.builder.components.Engine;
+import patterns.builder.components.GPSNavigator;
+import patterns.builder.components.Transmission;
+import patterns.builder.components.TripComputer;
+
+/**
+ * Руководство автомобиля — это второй продукт. Заметьте, что руководство и сам
+ * автомобиль не имеют общего родительского класса. По сути, они независимы.
+ */
+public class Manual {
+    private final CarType carType;
+    private final int seats;
+    private final Engine engine;
+    private final Transmission transmission;
+    private final TripComputer tripComputer;
+    private final GPSNavigator gpsNavigator;
+
+    public Manual(CarType carType, int seats, Engine engine, Transmission transmission,
+                  TripComputer tripComputer, GPSNavigator gpsNavigator) {
+        this.carType = carType;
+        this.seats = seats;
+        this.engine = engine;
+        this.transmission = transmission;
+        this.tripComputer = tripComputer;
+        this.gpsNavigator = gpsNavigator;
+    }
+
+    public String print() {
+        String info = "";
+        info += "Type of car: " + carType + "\n";
+        info += "Count of seats: " + seats + "\n";
+        info += "Engine: volume - " + engine.getVolume() + "; mileage - " + engine.getMileage() + "\n";
+        info += "Transmission: " + transmission + "\n";
+        if (this.tripComputer != null) {
+            info += "Trip Computer: Functional" + "\n";
+        } else {
+            info += "Trip Computer: N/A" + "\n";
+        }
+        if (this.gpsNavigator != null) {
+            info += "GPS Navigator: Functional" + "\n";
+        } else {
+            info += "GPS Navigator: N/A" + "\n";
+        }
+        return info;
+    }
+}
